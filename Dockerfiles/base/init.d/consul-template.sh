133351bbb721cc971448e73d8d0974e9fbbbac63
Alexey Kovrizhkin <jean@tender.pro>
2015-02-11 23:38:02 +0300

    fixed hangup when running start

--- Dockerfiles/base/init.d/consul-template.sh
+++ Dockerfiles/base/init.d/consul-template.sh
@@ -1,2 +1,10 @@
 
-consul-template -config=/etc/consul/templates/ -once
+# process templates in first run only
+# besause it will hang at second run
+# see https://github.com/hashicorp/consul-template/issues/188
+
+flag=/tmp/templates.done
+[ -f $flag ] || {
+  consul-template -config=/etc/consul/templates/ -once
+  touch $flag
+}
