07c1d7b260086ea65bf58caaab9ed91f6365ae14
Alexey Kovrizhkin <jean@tender.pro>
2015-04-29 16:13:17 +0300

    added prosody v0.1

--- Dockerfiles/nginx/init.d/setup_front.sh
+++ Dockerfiles/nginx/init.d/setup_front.sh
@@ -1,7 +1,8 @@
 
 DEST=/etc/nginx/sites-enabled/nginx-front.conf
 if [[ "$MODE" == "$FRONT_MODE" ]] ; then
+echo "Set front"
   # activate nginx front config in front mode
   [ ! -f $DEST ] && ln -s /etc/nginx/sites-available/nginx-front.conf $DEST
 fi
-
+echo "done"
