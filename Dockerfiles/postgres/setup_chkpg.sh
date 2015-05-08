0adc1cbf1a504f629aad268bef6ea10a44c35445
Alexey Kovrizhkin <jean@tender.pro>
2015-05-08 22:14:50 +0300

    postgres updates: * app setup code moved from Dockerfile to setup_*.sh * added dbcc support

---
+++ Dockerfiles/postgres/setup_chkpg.sh
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------------------------
+# Install check_postgres
+
+CHKPG_VER=2.21.0 && curl -OL http://bucardo.org/downloads/check_postgres-${CHKPG_VER}.tar.gz \
+  && tar -zxf check_postgres-${CHKPG_VER}.tar.gz \
+  && mv check_postgres-${CHKPG_VER}/check_postgres.pl /usr/local/bin/ \
+  && rm check_postgres-${CHKPG_VER}.tar.gz \
+  && rm -rf check_postgres-${CHKPG_VER}
---
+++ Dockerfiles/postgres/setup_dbcc.sh
@@ -0,0 +1,10 @@
+
+
+# -------------------------------------------------------------------------------
+# Install gosu - "run this specific application as this specific user and get out of the pipeline"
+
+VER=1.0 && curl -o /usr/local/bin/dbcc -SL https://github.com/LeKovr/dbcc/releases/download/$VER/dbcc_linux_amd64 \
+  && chmod +x /usr/local/bin/dbcc
+
+
+
