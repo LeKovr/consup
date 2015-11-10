f1feea74292d89f14a24aa5434abddbf25d96a6a
Alexey Kovrizhkin <lekovr@gmail.com>
2015-11-10 19:51:32 +0300

    install stable postgrest

---
+++ Dockerfiles/pgrest/setup_postgrest.sh
@@ -0,0 +1,11 @@
+
+# -------------------------------------------------------------------------------
+# Install Postgrest
+
+#https://github.com/begriffs/postgrest/releases/download/v0.2.12.0/postgrest-0.2.12.0-ubuntu.tar.xz
+
+USER=begriffs && PRJ=postgrest && VER=0.2.12.0 && NAME=${PRJ}-${VER}-ubuntu \
+  && curl -OL https://github.com/${USER}/${PRJ}/releases/download/v${VER}/${NAME}.tar.xz \
+  && tar -xaf ${NAME}.tar.xz \
+  && mv ${PRJ}-${VER} /usr/local/sbin/${PRJ} \
+  && rm ${PRJ}*
