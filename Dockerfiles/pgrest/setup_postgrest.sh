f514293b6e4e3d70029bd20199808fcc3dba76b8
Alexey Kovrizhkin <lekovr@gmail.com>
2015-11-10 22:11:08 +0300

    postgrest v0.2.12.0 has broken binary, revert to 0.2.11.1

--- Dockerfiles/pgrest/setup_postgrest.sh
+++ Dockerfiles/pgrest/setup_postgrest.sh
@@ -4,7 +4,7 @@
 
 #https://github.com/begriffs/postgrest/releases/download/v0.2.12.0/postgrest-0.2.12.0-ubuntu.tar.xz
 
-USER=begriffs && PRJ=postgrest && VER=0.2.12.0 && NAME=${PRJ}-${VER}-ubuntu \
+USER=begriffs && PRJ=postgrest && VER=0.2.11.1 && NAME=${PRJ}-${VER}-ubuntu \
   && curl -OL https://github.com/${USER}/${PRJ}/releases/download/v${VER}/${NAME}.tar.xz \
   && tar -xaf ${NAME}.tar.xz \
   && mv ${PRJ}-${VER} /usr/local/sbin/${PRJ} \
