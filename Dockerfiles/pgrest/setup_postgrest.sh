097e2e1f6c1f2537b7b918dee62a12f7454141e9
Alexey Kovrizhkin <lekovr@gmail.com>
2015-11-12 22:09:19 +0300

    fixed postgrest url

--- Dockerfiles/pgrest/setup_postgrest.sh
+++ Dockerfiles/pgrest/setup_postgrest.sh
@@ -2,10 +2,10 @@
 # -------------------------------------------------------------------------------
 # Install Postgrest
 
-#https://github.com/begriffs/postgrest/releases/download/v0.2.12.0/postgrest-0.2.12.0-ubuntu.tar.xz
+# https://github.com/begriffs/postgrest/releases/download/v0.2.11.1/postgrest-0.2.11.1-linux.tar.xz
 
-USER=begriffs && PRJ=postgrest && VER=0.2.11.1 && NAME=${PRJ}-${VER}-ubuntu \
+USER=begriffs && PRJ=postgrest && VER=0.2.11.1 && NAME=${PRJ}-${VER}-linux \
   && curl -OL https://github.com/${USER}/${PRJ}/releases/download/v${VER}/${NAME}.tar.xz \
   && tar -xaf ${NAME}.tar.xz \
-  && mv ${PRJ}-${VER} /usr/local/sbin/${PRJ} \
+  && sudo mv ${PRJ}-${VER} /usr/local/sbin/${PRJ} \
   && rm ${PRJ}*
