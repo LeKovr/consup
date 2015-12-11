ccf86bf2eb6be86c52f5f1e24a124e1110a7399b
Alexey Kovrizhkin <lekovr@gmail.com>
2015-12-11 19:48:54 +0300

    gogs setup upd2

--- Dockerfiles/gogs/init.d/user.sh
+++ Dockerfiles/gogs/init.d/user.sh
@@ -1,13 +1,16 @@
 
 # Create user & his home dir
 
-if [[ "$APPUSER" ]]; then
-  home=/home/app/$APPUSER
-  # Check if user exists already
-  # grep -qe "^$APPUSER:" /etc/passwd || useradd -m -r -s /bin/bash -Gwww-data -gusers -gsudo $APPUSER
-  grep -qe "^$APPUSER:" /etc/passwd || useradd -d $home $APPUSER
-  if [ ! -d $home ] ; then
-    mkdir -p $home
-   chown $APPUSER $home
-  fi
-fi
+home=/home/app/$APPUSER
+
+# Check if user exists already
+# grep -qe "^$APPUSER:" /etc/passwd || useradd -m -r -s /bin/bash -Gwww-data -gusers -gsudo $APPUSER
+grep -qe "^$APPUSER:" /etc/passwd || useradd -d $home $APPUSER
+
+# DEST Used by gogs install
+# This will be removed after switch to consul-template >= 0.11.1
+DEST=/opt/gogs/custom/conf
+chown -R $APPUSER $DEST
+
+# Mount point owner
+chown $APPUSER /home/app
