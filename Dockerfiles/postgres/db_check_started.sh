fb1ebf12fa32606ff407327d70cbc0b5ee3db5f8
Alexey Kovrizhkin <lekovr@gmail.com>
2015-12-16 23:45:54 +0300

    PG start checking improved

--- Dockerfiles/postgres/db_check_started.sh
+++ Dockerfiles/postgres/db_check_started.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# Wait for postgres and start dependent services
+log() {
+  D=$(date "+%F %T")
+  echo  "$D $1"
+}
+
 # Check when DB begins accept connections & reload consul after
-echo "Wait for PG"
+log "Wait for PG"
 while ! gosu postgres check_postgres.pl --action connection >/dev/null ; do
 sleep 1
 done
+#echo "PG ready. Start Consul"
+#supervisorctl -c /etc/supervisor/supervisord.conf start consul
 
-echo "PG ready. Start Consul"
-supervisorctl -c /etc/supervisor/supervisord.conf start consul
+log "Slow check"
+# Change healh check interval
+sed -i '/"interval": "1s"/c       "interval": "1m"' /etc/consul/src/postgres.json
+consul reload
 
-echo "Start dbcc"
+log "Start dbcc"
 supervisorctl -c /etc/supervisor/supervisord.conf start dbcc
 
-echo "Done"
+log "Done"
 # Say all is Ok to supervisor
 exit 0
