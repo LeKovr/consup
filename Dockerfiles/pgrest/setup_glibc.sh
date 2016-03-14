fd742cdb362c6e3662c430c01c488c5ea5779435
Alexey Kovrizhkin <lekovr@gmail.com>
2016-03-14 10:38:08 +0300

    locale fix

--- Dockerfiles/pgrest/setup_glibc.sh
+++ Dockerfiles/pgrest/setup_glibc.sh
@@ -4,4 +4,4 @@
 # postgrest 0.3.0.1 needc glibc >= 2.14
 echo "deb http://ftp.debian.org/debian experimental main" >> /etc/apt/sources.list
 apt-get update
-apt-get -t experimental -y install libc6
+apt-get -t experimental -y install libc6 locales
