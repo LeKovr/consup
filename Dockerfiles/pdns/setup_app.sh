02de059bbf911946dfd954bffd4d5270470d9f7a
Alexey Kovrizhkin <lekovr@gmail.com>
2016-04-06 19:47:11 +0300

    powerdns setup update

--- Dockerfiles/pdns/setup_app.sh
+++ Dockerfiles/pdns/setup_app.sh
@@ -2,8 +2,12 @@
 # -------------------------------------------------------------------------------
 # Install App
 
+CONF=/etc/powerdns/pdns.conf
 apt-get update \
   && apt-get -y install \
     postgresql-client \
     pdns-backend-pgsql \
-  && rm -f /etc/powerdns/pdns.d/pdns.simplebind
+  && rm -f /etc/powerdns/pdns.d/pdns.simplebind \
+  && sed -ri "s/^#\s*(webserver=)no/\1 yes/" $CONF \
+  && sed -ri "s/^#\s*(webserver-address=)127.0.0.1/\1 0.0.0.0/" $CONF
+
