e99a2ccd63d28737b3b2ed626490370e3f0badaa
Alexey Kovrizhkin <lekovr@gmail.com>
2016-04-07 16:49:01 +0300

    v 0.3.1.1

--- Dockerfiles/pgrest/setup_postgrest.sh
+++ Dockerfiles/pgrest/setup_postgrest.sh
@@ -5,7 +5,7 @@
 # https://github.com/begriffs/postgrest/releases/download/v0.2.11.1/postgrest-0.2.11.1-linux.tar.xz
 #  https://github.com/begriffs/postgrest/releases/download/v0.3.0.1/postgrest-0.3.0.1-ubuntu.tar.xz
 
-USER=begriffs && PRJ=postgrest && VER=0.3.1.0 && NAME=${PRJ}-${VER}-ubuntu \
+USER=begriffs && PRJ=postgrest && VER=0.3.1.1 && NAME=${PRJ}-${VER}-ubuntu \
   && curl -OL https://github.com/${USER}/${PRJ}/releases/download/v${VER}/${NAME}.tar.xz \
   && tar -xaf ${NAME}.tar.xz \
   && mv ${PRJ} /usr/local/sbin/${PRJ} \
