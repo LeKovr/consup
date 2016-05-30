01079144b0e8577b169ffd00a8fcd5ec6ad2aa2d
Alexey Kovrizhkin <lekovr@gmail.com>
2016-05-30 17:19:34 +0300

    webhook v2.3.8

--- Dockerfiles/webhook/setup_app.sh
+++ Dockerfiles/webhook/setup_app.sh
@@ -4,7 +4,7 @@
 
 # https://github.com/LeKovr/webhook/releases/download/2.3.6/webhook_linux_amd64.tar.gz
 
-VENDOR=LeKovr && PRJ=webhook && VER=2.3.6 && NAME=${PRJ}_linux_amd64 \
+VENDOR=LeKovr && PRJ=webhook && VER=2.3.8 && NAME=${PRJ}_linux_amd64 \
   && curl -OL https://github.com/$VENDOR/$PRJ/releases/download/$VER/$NAME.tar.gz \
   && tar -xf ${NAME}.tar.gz \
   && mv ${NAME} /usr/local/sbin/${PRJ} \
