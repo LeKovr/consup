520a08fc537c66d3f2341da1fe92c9ad3645ae97
Alexey Kovrizhkin <lekovr@gmail.com>
2016-06-01 14:27:23 +0300

    dbcc v1.6

--- Dockerfiles/postgres/setup_dbcc.sh
+++ Dockerfiles/postgres/setup_dbcc.sh
@@ -4,9 +4,9 @@
 # Install dbcc - "Database superuser agent: Listen http port, 
 #   check if given database & user exists and create them otherwise"
 
-# https://github.com/LeKovr/dbcc/releases/download/v1.4/dbcc_linux_amd64.zip
+# https://github.com/LeKovr/dbcc/releases/download/v1.6/dbcc_linux_amd64.zip
 
-VER=v1.4 && NAME=dbcc_linux_$(dpkg --print-architecture) \
+VER=v1.6 && NAME=dbcc_linux_$(dpkg --print-architecture) \
   && curl -OL https://github.com/LeKovr/dbcc/releases/download/${VER}/$NAME.zip \
   && unzip $NAME.zip && rm $NAME.zip \
   && mv $NAME /usr/local/bin/dbcc
