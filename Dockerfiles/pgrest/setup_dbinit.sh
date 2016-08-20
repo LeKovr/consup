a7706553dacd9921534bf4882a0739b63f8f318d
Alexey Kovrizhkin <jean@tender.pro>
2016-08-20 23:51:14 +0300

    minor fix

---
+++ Dockerfiles/pgrest/setup_dbinit.sh
@@ -0,0 +1,17 @@
+
+
+# -------------------------------------------------------------------------------
+# Install dbinit - database init script
+
+ver=pgrest-160818
+
+# source repo
+prg=consup
+branch=master
+url=https://raw.githubusercontent.com/LeKovr/$prg
+name=dbinit.sh
+
+echo "Setup $name from $prg $branch" \
+  && curl -OL $url/$branch/$name \
+  && chmod a+x $name \
+  && mv $name /usr/local/sbin/
