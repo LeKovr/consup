a414dffca4810a0ce532d1cc948fa2e56a571a4f
Alexey Kovrizhkin <jean@tender.pro>
2016-11-17 23:15:13 +0300

    download from github now

--- Dockerfiles/gogs/setup_app.sh
+++ Dockerfiles/gogs/setup_app.sh
@@ -7,13 +7,14 @@
 #https://dl.gogs.io/gogs_v0.7.33_linux_amd64.tar.gz
 #https://github.com/gogits/gogs/releases/download/v0.9.46/linux_amd64.tar.gz
 #https://dl.gogs.io/gogs_v0.9.46_linux_amd64.tar.gz
+#https://github.com/gogits/${PRJ}/releases/download/${VER}/linux_amd64.tar.gz
 
 VER=v0.9.97 && PRJ=gogs \
   && echo "Setup $PRJ $VER" \
   && NAME=${PRJ}_${VER}_linux_amd64 \
-  && curl -OL https://dl.gogs.io/${NAME}.tar.gz \
-  && tar -xaf ${NAME}.tar.gz -C /opt/ \
-  && rm ${NAME}.tar.gz
+  && curl -OL https://github.com/gogits/${PRJ}/releases/download/${VER}/linux_amd64.tar.gz \
+  && tar -xzf linux_amd64.tar.gz -C /opt \
+  && rm linux_amd64.tar.gz
 
 #https://packager.io/gh/pkgr/gogs/install?bid=377#debian-7-gogs
 
