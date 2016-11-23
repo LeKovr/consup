eb98a6cce7b64e9988b573f124d4e5ebac0ed3cc
Alexey Kovrizhkin <jean@tender.pro>
2016-11-24 00:41:07 +0300

    webhook author started prepare binaries

--- Dockerfiles/webhook/setup_app.sh
+++ Dockerfiles/webhook/setup_app.sh
@@ -3,8 +3,9 @@
 # Install App
 
 # https://github.com/LeKovr/webhook/releases/download/2.3.6/webhook_linux_amd64.tar.gz
+# https://github.com/adnanh/webhook/releases/download/2.6.0/webhook-linux-amd64.tar.gz
 
-VENDOR=LeKovr && PRJ=webhook && VER=2.3.8 && NAME=${PRJ}_linux_amd64 \
+VENDOR=adnanh && PRJ=webhook && VER=2.6.0 && NAME=${PRJ}-linux-amd64 \
   && curl -OL https://github.com/$VENDOR/$PRJ/releases/download/$VER/$NAME.tar.gz \
   && tar -xf ${NAME}.tar.gz \
   && mv ${NAME} /usr/local/sbin/${PRJ} \
