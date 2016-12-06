01d35914c6e4078d661b0f78b09255f0aab571a3
Alexey Kovrizhkin <jean@tender.pro>
2016-12-06 22:21:51 +0300

    webtail v0.6

--- Dockerfiles/webhook/init.d/consup_ln.sh
+++ Dockerfiles/webhook/init.d/consup_ln.sh
@@ -1,7 +1,7 @@
 
 # Create consup symlink
-if [ -d /home/consup ] ; then
-  if [ ! -L /home/app/consup ] ; then
-    ln -s /home/consup /home/app/consup
-  fi
-fi
+#if [ -d /home/consup ] ; then
+#  if [ ! -L /home/app/consup ] ; then
+#    ln -s /home/consup /home/app/consup
+#  fi
+#fi
--- Dockerfiles/webhook/setup_webtail.sh
+++ Dockerfiles/webhook/setup_webtail.sh
@@ -3,7 +3,7 @@
 # Install WebTail
 # https://github.com/LeKovr/webtail/releases/download/v0.1/webtail_linux_amd64.zip
 
-VER=0.5 && NAME=webtail_linux_$(dpkg --print-architecture) \
+VER=0.6 && NAME=webtail_linux_$(dpkg --print-architecture) \
   && curl -OL https://github.com/LeKovr/webtail/releases/download/v${VER}/${NAME}.zip \
   && unzip ${NAME}.zip \
   && rm ${NAME}.zip README.md \
