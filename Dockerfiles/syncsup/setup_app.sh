ca76cac265f293fc5dee816b52099b336feff4f3
Alexey Kovrizhkin <jean@tender.pro>
2017-03-08 20:29:07 +0300

    version names update2

--- Dockerfiles/syncsup/setup_app.sh
+++ Dockerfiles/syncsup/setup_app.sh
@@ -4,6 +4,9 @@
 #https://github.com/syncthing/discosrv/releases/download/v0.13.5/discosrv-linux-amd64-v0.13.5.tar.gz
 #https://github.com/syncthing/relaysrv/releases/download/v0.13.5/relaysrv-linux-amd64-v0.13.5.tar.gz
 
+#https://github.com/syncthing/discosrv/releases/download/v0.14.5/stdiscosrv-linux-amd64-v0.14.5.tar.gz
+#https://github.com/syncthing/relaysrv/releases/download/v0.14.5/strelaysrv-linux-amd64-v0.14.5.tar.gz
+
 USER=syncthing
 VER=0.14.5
 
@@ -11,9 +14,9 @@ setup() {
   local prj=$1
   local name=${prj}-linux-amd64-v${VER}
   echo "Setup $prj v $VER"
-  curl -OL https://github.com/${USER}/$prj/releases/download/v${VER}/$name.tar.gz \
-    && tar -xaf $name.tar.gz \
-    && mv $name/$prj /usr/local/sbin/$prj \
+  curl -OL https://github.com/${USER}/$prj/releases/download/v${VER}/st$name.tar.gz \
+    && tar -xaf st$name.tar.gz \
+    && mv st$name/st$prj /usr/local/sbin/$prj \
     && rm -rf $name*
 }
 
