074ef510b168579e657b48fa7ee15ab91637875a
Alexey Kovrizhkin <jean@tender.pro>
2017-03-19 17:16:32 +0300

    consul 0.7.5

--- Dockerfiles/base/setup_consul.sh
+++ Dockerfiles/base/setup_consul.sh
@@ -3,7 +3,7 @@
 # Install Consul - tool for service discovery, monitoring and configuration
 # http://brian.akins.org/blog/2014/05/03/simple-consul-example/
 
-VER=0.7.1 && NAME=consul_${VER}_linux_$(dpkg --print-architecture) \
+VER=0.7.5 && NAME=consul_${VER}_linux_$(dpkg --print-architecture) \
   && curl -OL https://releases.hashicorp.com/consul/${VER}/${NAME}.zip \
   && unzip ${NAME}.zip \
   && rm ${NAME}.zip \
