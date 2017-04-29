f9b5f080daf4b4837ac1efe7b1072169b9a74f50
Alexey Kovrizhkin <jean@tender.pro>
2017-04-29 20:40:07 +0300

    consul & postgrest update

--- Dockerfiles/pgrest/setup_app.sh
+++ Dockerfiles/pgrest/setup_app.sh
@@ -3,10 +3,10 @@
 # Install Postgrest
 
 # https://github.com/begriffs/postgrest/releases/download/v0.2.11.1/postgrest-0.2.11.1-linux.tar.xz
-#  https://github.com/begriffs/postgrest/releases/download/v0.3.0.1/postgrest-0.3.0.1-ubuntu.tar.xz
+# https://github.com/begriffs/postgrest/releases/download/v0.4.0.0/postgrest-0.4.0.0-ubuntu.tar.xz
 
-USER=begriffs && PRJ=postgrest && VER=0.3.2.0 && NAME=${PRJ}-${VER}-ubuntu
-echo "Setup $PRJ v $VER"
+USER=begriffs && PRJ=postgrest && VER=0.4.0.0 && NAME=${PRJ}-${VER}-ubuntu
+echo "Setup $PRJ v$VER"
 curl -OL https://github.com/${USER}/${PRJ}/releases/download/v${VER}/${NAME}.tar.xz \
   && tar -xaf ${NAME}.tar.xz \
   && mv ${PRJ} /usr/local/sbin/${PRJ} \
