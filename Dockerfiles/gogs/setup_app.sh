8ef6f6a136bd5f899da7f4f9ac448025bd8c5af9
Alexey Kovrizhkin <jean@tender.pro>
2017-04-29 21:02:23 +0300

    consul & gogs update

--- Dockerfiles/gogs/setup_app.sh
+++ Dockerfiles/gogs/setup_app.sh
@@ -4,7 +4,7 @@
 
 #https://github.com/gogits/${PRJ}/releases/download/${VER}/linux_amd64.tar.gz
 
-VER=v0.9.97 && PRJ=gogs \
+VER=v0.11.4 && PRJ=gogs \
   && echo "Setup $PRJ $VER" \
   && NAME=${PRJ}_${VER}_linux_amd64 \
   && curl -OL https://github.com/gogits/${PRJ}/releases/download/${VER}/linux_amd64.tar.gz \
