66420eb3e03219b07580e51af268ed372043daf0
Alexey Kovrizhkin <jean@tender.pro>
2017-08-18 13:03:36 +0300

    dbrpc 0.24

--- Dockerfiles/pgrest/setup_dbrpc.sh
+++ Dockerfiles/pgrest/setup_dbrpc.sh
@@ -3,7 +3,7 @@
 # -------------------------------------------------------------------------------
 # Install dbrpc - RPC server for database stored procedures
 
-PRJ=dbrpc && VER=0.23
+PRJ=dbrpc && VER=0.24
 echo "Setup $PRJ v$VER"
 NAME=${PRJ}_linux_$(dpkg --print-architecture) \
   && curl -OL https://github.com/LeKovr/dbrpc/releases/download/v${VER}/$NAME.zip \
